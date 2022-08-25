7451e0a0e950d646b9a38ac6dcc7cf49b194a680
GMohammadAli <gulammohammadali@gmail.com>
2022-08-26 00:12:03 +0530

    CSV File Part Done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import { CSVLink } from "react-csv";
+import axios from 'axios';
+import { Button } from '@mui/material';
+
 
 function App() {
+  const [blogs, setBlogs] = useState();
+  const [areBlogsAvailable , setAreBlogsAvailable] = useState(false);
+
+  const headers = {
+    Authorization: "eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoyLCJleHAiOjE2NjIwNTU0NzF9.nVgzJofZqhe6CN31pwAi8rdqrwCQRWq6pL7rV-8cqr0"
+  };
+
+  //This is an API call on my BLogs_App_Backend Repository
+  const getBlogs = async () => {
+    await axios
+      .get(`${process.env.REACT_APP_API_KEY}/users/2/blogs`, {headers})
+      .then((res) => {
+        setBlogs(res.data);
+        setAreBlogsAvailable(true);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  useEffect(() => {
+    getBlogs();
+    // eslint-disable-next-line
+  }, [])
+
+  const csvHeaders = [
+    {
+      label:"Sr. No", key:"id"
+    },
+    {
+      label:"Title", key:"title"
+    },
+    {
+      label:"Description", key:"description"
+    },
+    {
+      label:"Is Liked?", key:"isLiked"
+    }
+  ]
+
+
+  const csvLink = {
+    headers: csvHeaders,
+    data: blogs,
+    filename: "blogs.csv"
+  };
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {areBlogsAvailable && (
+        <CSVLink {...csvLink}>
+          <Button
+            sx={{ color: "#FFF !important" ,textDecorationLine:"none"}}
+            color="primary"
+            variant="contained"
+          >
+            Download Blogs File
+          </Button>
+        </CSVLink>
+      )}
     </div>
   );
 }
 
-export default App;
+export default App
+
